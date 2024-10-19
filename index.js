8e42b328f3e5022bbc77629199b6ba17b23400d6
GeovanneOliver <geo-san@hotmail.com>
2024-10-19 12:14:04 -0300

    Calculadora em JS

---
+++ index.js
@@ -0,0 +1,19 @@
+const tela = document.getElementById("tela");
+
+function mostrarNaTela(input){
+    tela.value += input;
+}
+
+function limpar() {
+    tela.value = "";
+}
+
+
+function calcular() {
+    try{
+        tela.value = eval(tela.value);
+    }
+    catch(erro){
+        tela.value = "Erro";
+    }   
+}
